18fbb883c28308923eaf15b5141ffcfe9b31e990
Hung619 <n.d.h.hung2004@gmail.com>
2025-06-25 21:56:49 +0700

    ....

---
+++ TieuLuanQuanNet/NhanVien/frmGiaoDienNhanVien.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace TieuLuanQuanNet
+{
+    partial class frmGiaoDienNhanVien
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btThongKeDoanhThu = new System.Windows.Forms.Button();
+            this.btQuanLyTaiKhoan = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(186, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(934, 97);
+            this.panel2.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TieuLuanQuanNet.Properties.Resources.OIP__2_;
+            this.pictureBox1.Location = new System.Drawing.Point(43, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 94);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(159, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(325, 40);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Giao Diện Nhân Viên";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Controls.Add(this.btThongKeDoanhThu);
+            this.panel3.Controls.Add(this.btQuanLyTaiKhoan);
+            this.panel3.Location = new System.Drawing.Point(0, 98);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(188, 536);
+            this.panel3.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.button1.Location = new System.Drawing.Point(12, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 58);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Máy Cho Thuê";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btThongKeDoanhThu
+            // 
+            this.btThongKeDoanhThu.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btThongKeDoanhThu.Location = new System.Drawing.Point(12, 133);
+            this.btThongKeDoanhThu.Name = "btThongKeDoanhThu";
+            this.btThongKeDoanhThu.Size = new System.Drawing.Size(164, 58);
+            this.btThongKeDoanhThu.TabIndex = 4;
+            this.btThongKeDoanhThu.Text = "Đăng Xuất";
+            this.btThongKeDoanhThu.UseVisualStyleBackColor = true;
+            this.btThongKeDoanhThu.Click += new System.EventHandler(this.btThongKeDoanhThu_Click);
+            // 
+            // btQuanLyTaiKhoan
+            // 
+            this.btQuanLyTaiKhoan.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btQuanLyTaiKhoan.Location = new System.Drawing.Point(12, 69);
+            this.btQuanLyTaiKhoan.Name = "btQuanLyTaiKhoan";
+            this.btQuanLyTaiKhoan.Size = new System.Drawing.Size(164, 58);
+            this.btQuanLyTaiKhoan.TabIndex = 3;
+            this.btQuanLyTaiKhoan.Text = "Đổi Mật Khẩu";
+            this.btQuanLyTaiKhoan.UseVisualStyleBackColor = true;
+            this.btQuanLyTaiKhoan.Click += new System.EventHandler(this.btQuanLyTaiKhoan_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(188, 97);
+            this.panel1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NETMAN";
+            // 
+            // frmGiaoDienNhanVien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(796, 451);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmGiaoDienNhanVien";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Giao diện nhân viên";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btThongKeDoanhThu;
+        private System.Windows.Forms.Button btQuanLyTaiKhoan;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ TieuLuanQuanNet/NhanVien/frmGiaoDienNhanVien.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TieuLuanQuanNet
+{
+    public partial class frmGiaoDienNhanVien : Form
+    {
+        public frmGiaoDienNhanVien()
+        {
+            InitializeComponent();
+        }
+
+        private void btQuanLyNhanVien_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btQuanLyTaiKhoan_Click(object sender, EventArgs e)
+        {
+            (new frmDoiMatKhau()).ShowDialog();
+
+        }
+
+        private void btThongKeDoanhThu_Click(object sender, EventArgs e)
+        {
+            (new frmDangNhap()).ShowDialog();
+            this.Close();
+        }
+
+        private void btThongKeNhanVien_Click(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            (new frmMayChoThue()).ShowDialog();
+
+        }
+    }
+}
